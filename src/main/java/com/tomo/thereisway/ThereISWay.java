43c5e49d98ffa0c4dc148e84ec659eac6b8ef200
TOMO <Rouczyk@gmail.com>
2022-08-20 15:02:26 +0200

    DV-TSW-00 - Configure project.
    * First setup of plugin project.

---
+++ src/main/java/com/tomo/thereisway/ThereISWay.java
@@ -0,0 +1,16 @@
+package com.tomo.thereisway;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ThereISWay extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
