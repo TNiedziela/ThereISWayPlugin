9dd8cf2420938d4a13cf409a7741e3c70ac12871
Tomasz Niedziela <Rouczyk@gmail.com>
2023-06-08 17:30:02 +0200

    DV-TSW-10 Effects on player after teleporting to waypoint. * Added effects applied on player after running move command.

--- src/main/java/com/tomo/thereisway/management/commands/WaypointCommand.java
+++ src/main/java/com/tomo/thereisway/management/commands/WaypointCommand.java
@@ -6,11 +6,15 @@ import com.tomo.thereisway.management.utilities.WaypointCommandTabCompleter;
 import com.tomo.thereisway.management.waypoints.WaypointManagementService;
 import com.tomo.thereisway.waypoints.PlayerWaypoint;
 import com.tomo.thereisway.waypoints.ServerWaypoint;
+import com.tomo.thereisway.waypoints.Waypoint;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
@@ -67,9 +71,17 @@ public class WaypointCommand implements CommandExecutor {
             player.sendMessage("You don't have waypoint with such name (" + waypointName + ")");
         } else {
             PlayerWaypoint waypoint = playerWaypoint.get();
-            player.teleport(waypoint.getPlacement());
-            player.sendMessage(ChatUtils.asGreenMessage("Successfully moved to: " + waypointName));
+            movePlayerToWaypointAndApplyEffects(player, waypoint, 30);
         }
+    }
+
+    private void movePlayerToWaypointAndApplyEffects(Player player, Waypoint waypoint, int moveDurationInTicks) {
+        player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, moveDurationInTicks, 20));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, moveDurationInTicks, 20));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, moveDurationInTicks, 1000));
+        Bukkit.getScheduler().runTaskLater(this.plugin, () -> player.teleport(waypoint.getPlacement()), moveDurationInTicks);
+        Bukkit.getScheduler().runTaskLater(this.plugin,
+                () -> player.sendMessage(ChatUtils.asGreenMessage("Successfully moved to: " + waypoint.getWaypointName())), 2);
     }
 
     private void movePlayerToServerWaypointIfPossible(Player player, String waypointName) {
